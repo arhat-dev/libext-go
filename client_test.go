5f6549efc5f3e1e17154487708a6d0eab296f90c
Jeffrey Stoke <me@arhat.dev>
2020-11-06 17:52:48 +0100

    Fix pipe name generation for windows

--- client_test.go
+++ client_test.go
@@ -100,7 +100,7 @@ func TestClient_ProcessNewStream(t *testing.T) {
 			case test.network == "pipe":
 				switch runtime.GOOS {
 				case "windows":
-					listenAddr = "pipe://test-" + test.network
+					listenAddr = `\\.\pipe\test-` + test.network
 				default:
 					listenAddr, err = iohelper.TempFilename(os.TempDir(), "*")
 					if !assert.NoError(t, err) {
